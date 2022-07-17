56605e86633fdc78b7e70ce8bc7588fb817a1d2d
Костя Иванчик <kostya.ivanchik@mail.ru>
2022-07-17 16:34:11 +0300

    Added Routes in Client

--- client/src/components/AppRouter.js
+++ client/src/components/AppRouter.js
@@ -1,18 +1,22 @@
-import React from 'react';
-import {Switch, Route, Redirect, Router} from 'react-router-dom'
+import React, { Component } from 'react';
+import {Routes, Route, Redirect,Navigate, Router} from 'react-router-dom'
+import Shop from '../pages/Shop';
 import { authRoutes, publicRoutes } from '../routes';
+import { SHOP_ROUTE } from '../utils/consts';
+
 
 const AppRouter = () => {
     const isAuth = false
     return (
-        <Switch> 
+        <Routes>
             {isAuth && authRoutes.map(({path, Component}) =>
-             <Route key={path} path={path} component = {Component} exact/>
+            <Route key={path} path={path} element={<Component />} exact/>
             )}
             {publicRoutes.map(({path, Component}) =>
-             <Route key={path} path={path} component = {Component} exact/>
+            <Route key={path} path={path} element={<Component />} exact/>
             )}
-        </Switch>
+            <Route path="*" element={<Navigate to={SHOP_ROUTE} replace />}/>
+        </Routes>
     )
 }
 
