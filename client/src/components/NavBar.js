4c2a8c3be37513f2dfb8fae5de51f5d3b11ca533
Костя Иванчик <kostya.ivanchik@mail.ru>
2022-07-21 23:23:02 +0300

    added button autorization

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -6,22 +6,30 @@ import Container from 'react-bootstrap/Container';
 import { NavLink } from "react-router-dom";
 import { SHOP_ROUTE } from "../utils/consts";
 import Button from "react-bootstrap/Button"
+import {observer} from "mobx-react-lite"
 
-const NavBar = () => {
+
+const NavBar = observer (() => {
     const {user} = useContext(Context)
     return (
         <div>
             <Navbar bg="dark" variant="dark">
         <Container>
-          <NavLink style={{color:'white'}} to={SHOP_ROUTE}>КупиДевайс</NavLink>
-          <Nav className="ml-auto" style={{color:'white'}}>
-            <Button variant={"outline-light"}>Авторизация</Button>
+        <NavLink style={{color:'white'}} to={SHOP_ROUTE}>КупиДевайс</NavLink>
+          {user.isAuth ? 
+          <Nav style={{color:'white'}}>
             <Button variant={"outline-light"}>Админ панель</Button>
+            <Button variant={"outline-light"}className="ms-4">Выйти</Button>
           </Nav>
+          :
+          <Nav className="ml-auto" style={{color:'white'}}>
+            <Button className="ml-4" variant={"outline-light"} onClick={() => user.setIsAuth(true)}>Авторизация</Button>
+          </Nav>
+          }
         </Container>
       </Navbar>
       </div>
     );
-};
+});
 
 export default NavBar;
